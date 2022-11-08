dc3e0de31f426c76e49be92453c50895a6dcabc6
Bruno Hugo Knaesel <78325732+brunoknaesel@users.noreply.github.com>
2022-11-07 21:58:04 -0300

    Add files via upload

---
+++ bey-trabalho3/src/main/java/projeto/web/beytrabalho3/BeyTrabalho3Application.java
@@ -0,0 +1,13 @@
+package projeto.web.beytrabalho3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BeyTrabalho3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BeyTrabalho3Application.class, args);
+	}
+
+}
